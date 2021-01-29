d1debfb4ea9da77b6463adddd736c6fab0108a95
Atsushi Kataoka <atsushi.kataoka@msmc.jp>
2021-01-29 17:29:54 +0900

    Implements Add to Favorite

---
+++ Mmfm/ViewModels/FavoriteRegisterViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mmfm
+{
+    public class FavoriteRegisterViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        public FavoriteRegisterViewModel(string path)
+        {
+            FullPath = path;
+            FavoriteName = Path.GetFileName(FullPath);
+        }
+
+        public string FullPath
+        {
+            get;
+            private set;
+        }
+
+        private string favoriteName;
+        public string FavoriteName
+        {
+            get => favoriteName;
+            set
+            {
+                favoriteName = value;
+                OnPropertyChanged("FavoriteName");
+            }
+        }
+    }
+}
--- Mmfm/ViewModels/MainViewModel.cs
+++ Mmfm/ViewModels/MainViewModel.cs
@@ -13,7 +13,7 @@ namespace Mmfm
 
         public IEnumerable<InputBinding> InputBindings {
             get => commands.Select(c => (InputBinding)c);
-        } 
+        }
 
         private List<CommandItemViewModel> commands = new List<CommandItemViewModel>();
 
@@ -21,7 +21,22 @@ namespace Mmfm
         {
             return DualFileManager.ActiveFileManager.CurrentDirectory.FullPath.Length > 0;
         }
-        
+
+        private void ShowsAddFavorite()
+        {
+            var path = DualFileManager.ActiveFileManager.CurrentDirectory.FullPath;
+            var content = new FavoriteRegisterViewModel(path);
+            var dialog = new DialogViewModel { Content = content };
+            
+            Messenger.Default.Send(dialog);
+            if (dialog.Result == true)
+            {
+                var favorite = FileViewModel.CreateFavorite(path, content.FavoriteName, IconExtractor.Extract(path));
+                DualFileManager.First.Favorites.Add(favorite);
+                DualFileManager.Second.Favorites.Add(favorite);
+            }
+        }
+
         public MainViewModel()
         {
             commands.Add(new CommandItemViewModel("Back to parent", "Backspace", new RelayCommand(() => DualFileManager.ActiveFileManager.BackToParent(), CanExecute)));
@@ -32,7 +47,8 @@ namespace Mmfm
             commands.Add(new CommandItemViewModel("Select All", "Alt+U", new RelayCommand(() => DualFileManager.ActiveFileManager.SelectAll(), CanExecute)));
             commands.Add(new CommandItemViewModel("Deselect All", "Shift+Alt+U", new RelayCommand(() => DualFileManager.ActiveFileManager.DeselectAll(), CanExecute)));
             commands.Add(new CommandItemViewModel("Rename", "F2", new RelayCommand(() => DualFileManager.ActiveFileManager.RenameFiles(), CanExecute)));
- 
+            commands.Add(new CommandItemViewModel("Favorite", "Alt+F", new RelayCommand(() => ShowsAddFavorite(), CanExecute)));
+
             var commandsToRegister = commands.ToArray();
             commands.Add(new CommandItemViewModel("", "Ctrl+Shift+P", new RelayCommand(() => {
                 var content = new CommandPaletteViewModel(commandsToRegister);
