7e29fadff2bbd56102094bb49b21aadec15b9253
Atsushi Kataoka <atsushi.kataoka@msmc.jp>
2021-02-09 14:01:25 +0900

    最後にキーボードフォーカスを受け取ったListViewのみを対象とするように修正

--- Mmfm/Behaviors/ListViewFocusSelectedItemOnItemContainersGenerated.cs
+++ Mmfm/Behaviors/ListViewFocusSelectedItemOnItemContainersGenerated.cs
@@ -13,22 +13,31 @@ using System.Windows.Threading;
 namespace Mmfm.Behaviors
 {
     public class ListViewFocusSelectedItemOnItemContainersGenerated : Behavior<ListView>
-    {        
+    {
+        private static ListView lastFocusedListView = null;
+
         protected override void OnAttached()
         {
             base.OnAttached();
-            AssociatedObject.ItemContainerGenerator.StatusChanged += ItemContainerGenerator_StatusChanged;            
+            AssociatedObject.ItemContainerGenerator.StatusChanged += ItemContainerGenerator_StatusChanged;
+            AssociatedObject.IsKeyboardFocusWithinChanged += AssociatedObject_IsKeyboardFocusWithinChanged;
         }
 
         protected override void OnDetaching()
         {
             AssociatedObject.ItemContainerGenerator.StatusChanged -= ItemContainerGenerator_StatusChanged;
+            AssociatedObject.IsKeyboardFocusWithinChanged -= AssociatedObject_IsKeyboardFocusWithinChanged;
             base.OnDetaching();
         }
 
-        private void ItemContainerGenerator_StatusChanged(object sender, EventArgs e)
+        private void AssociatedObject_IsKeyboardFocusWithinChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
-            if (AssociatedObject.IsFocused && AssociatedObject.ItemContainerGenerator.Status == System.Windows.Controls.Primitives.GeneratorStatus.ContainersGenerated)
+            lastFocusedListView = AssociatedObject;
+        }
+
+        private void ItemContainerGenerator_StatusChanged(object sender, EventArgs e)
+        {            
+            if (AssociatedObject == lastFocusedListView && AssociatedObject.ItemContainerGenerator.Status == System.Windows.Controls.Primitives.GeneratorStatus.ContainersGenerated)
             {
                 // https://stackoverflow.com/questions/7366961/listbox-scrollintoview-when-using-collectionviewsource-with-groupdescriptions-i
                 Dispatcher.BeginInvoke(DispatcherPriority.Render, new Action(() =>
