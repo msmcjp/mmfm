5d113059b3534a18e4b8ba3159a489ae2b4974fe
Atsushi Kataoka <atsushi.kataoka@msmc.jp>
2021-02-19 11:54:55 +0900

    Add `Open settings file` command

--- Mmfm/Plugins/Controller.cs
+++ Mmfm/Plugins/Controller.cs
@@ -2,6 +2,7 @@
 using Msmc.Patterns.Messenger;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Windows;
 
 namespace Mmfm.Plugins
@@ -24,6 +25,7 @@ namespace Mmfm.Plugins
             new CommandItemViewModel("Show hidden files", "Alt+Z", new RelayCommand(() => FileManagerSettings.ShowHiddenFiles = true, () => FileManagerSettings.ShowHiddenFiles == false)),
             new CommandItemViewModel("Hide hidden files", "Alt+Shift+Z", new RelayCommand(() => FileManagerSettings.ShowHiddenFiles = false, () => FileManagerSettings.ShowHiddenFiles == true)),
             new CommandItemViewModel("Quit", "Alt+F4", new RelayCommand(() => Application.Current.Shutdown(), () => true)),
+            new CommandItemViewModel("Open settings file", "Ctrl+,", new RelayCommand(() => Process.Start(new ProcessStartInfo(App.SettingsPath) { UseShellExecute = true }))),
         };
 
         public IMessenger Messenger
