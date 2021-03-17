2831053de773045de28347322d503a8c677d2173
Atsushi Kataoka <atsushi.kataoka@msmc.jp>
2021-03-17 14:51:12 +0900

    Fix crash bug.

--- Mmfm/Interactions/ListViewExclusiveSelectionBehavior.cs
+++ Mmfm/Interactions/ListViewExclusiveSelectionBehavior.cs
@@ -3,14 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Threading;
+using System.Linq;
 
 namespace Mmfm
 {
     public class ListViewExclusiveSelectionBehavior : Behavior<ListView>
     {
         private static ListView lastFocusedListView = null;
-        private static Dictionary<ListView, int> lastSelectedIndex = new Dictionary<ListView, int>();
+        private static Dictionary<ListView, object> lastSelectedItems = new Dictionary<ListView, object>();
 
         protected override void OnAttached()
         {
@@ -18,7 +20,7 @@ namespace Mmfm
             AssociatedObject.ItemContainerGenerator.StatusChanged += ItemContainerGenerator_StatusChanged;
             AssociatedObject.IsKeyboardFocusWithinChanged += AssociatedObject_IsKeyboardFocusWithinChanged;
             AssociatedObject.SelectionChanged += AssociatedObject_SelectionChanged;
-            lastSelectedIndex.Add(AssociatedObject, -1);
+            lastSelectedItems.Add(AssociatedObject, null);
         }
 
         protected override void OnDetaching()
@@ -26,42 +28,58 @@ namespace Mmfm
             AssociatedObject.SelectionChanged -= AssociatedObject_SelectionChanged;
             AssociatedObject.ItemContainerGenerator.StatusChanged -= ItemContainerGenerator_StatusChanged;
             AssociatedObject.IsKeyboardFocusWithinChanged -= AssociatedObject_IsKeyboardFocusWithinChanged;
-            lastSelectedIndex.Remove(AssociatedObject);
+            lastSelectedItems.Remove(AssociatedObject);
             base.OnDetaching();
+        }
+
+        private ListView LastFocusedListView
+        {
+            get => lastFocusedListView;
+            set
+            {                
+                foreach(var listView in lastSelectedItems.Keys.Where(x => x != value))
+                {
+                    listView.SelectedItem = null;
+                }
+                lastFocusedListView = value;
+                if(lastFocusedListView == AssociatedObject)
+                {
+                    lastFocusedListView.SelectedItem = lastSelectedItems[lastFocusedListView];
+                }
+            }
         }
 
         private void AssociatedObject_IsKeyboardFocusWithinChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
             if((bool)e.NewValue == true)
             {
-                lastFocusedListView = AssociatedObject;
-                AssociatedObject.SelectedIndex = lastSelectedIndex[AssociatedObject];
-            }
-            else
-            {
-                lastSelectedIndex[AssociatedObject] = AssociatedObject.SelectedIndex;
-                AssociatedObject.SelectedIndex = -1;
-            }
+                LastFocusedListView = AssociatedObject;
+            }    
         }
 
         private void ItemContainerGenerator_StatusChanged(object sender, EventArgs e)
         {            
-            if (AssociatedObject == lastFocusedListView && AssociatedObject.ItemContainerGenerator.Status == System.Windows.Controls.Primitives.GeneratorStatus.ContainersGenerated)
+            if (AssociatedObject == LastFocusedListView &&
+                AssociatedObject.ItemContainerGenerator.Status == GeneratorStatus.ContainersGenerated)
             {
                 // https://stackoverflow.com/questions/7366961/listbox-scrollintoview-when-using-collectionviewsource-with-groupdescriptions-i
                 Dispatcher.BeginInvoke(DispatcherPriority.Render, new Action(() =>
                 {
-                    var container = AssociatedObject.ItemContainerGenerator.ContainerFromIndex(AssociatedObject.SelectedIndex);
-                    (container as UIElement)?.Focus();
+                    var container = AssociatedObject.ItemContainerGenerator.ContainerFromItem(AssociatedObject.SelectedItem);
+                    (container as UIElement)?.Focus();                    
                 }));
             }
         }
 
         private void AssociatedObject_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (AssociatedObject != lastFocusedListView && AssociatedObject.SelectedIndex != -1)
+            if (LastFocusedListView == AssociatedObject)
             {
-                AssociatedObject.SelectedIndex = -1;
+                lastSelectedItems[AssociatedObject] = AssociatedObject.SelectedItem;
+            }
+            else if(AssociatedObject.SelectedItem != null)
+            {
+                AssociatedObject.SelectedItem = null;
             }
         }
     }
