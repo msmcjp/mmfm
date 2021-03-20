4f3e7bcf592e6ba4e5630441d4ae9edd9fdb785d
Atsushi Kataoka <atsushi.kataoka@msmc.jp>
2021-03-20 15:07:24 +0900

    Implements INotifyDataErrorInfo.

---
+++ Mmfm/ValidFileNameAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mmfm
+{
+    public class ValidFileNameAttribute : ValidationAttribute
+    {
+        private bool IsValidFileName(string s) => !Path.GetInvalidFileNameChars().Any(c => s.Contains(c));
+
+        public override bool IsValid(object value)
+        {
+            if (value is string == false)
+            {
+                return false;
+            }
+            return IsValidFileName((string)value);
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is string == false || IsValidFileName((string)value) == false)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
--- Mmfm/ViewModels/ContentDialogViewModel.cs
+++ Mmfm/ViewModels/ContentDialogViewModel.cs
@@ -2,20 +2,51 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows.Input;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
 
 namespace Mmfm
 {
-    public abstract class ContentDialogViewModel : INotifyPropertyChanged
+    public abstract class ContentDialogViewModel : INotifyPropertyChanged, INotifyDataErrorInfo
     {        
         #region INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void OnPropertyChanged(string propertyName)
+        protected void OnPropertyChanged([CallerMemberName]string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            Validate(propertyName);
         }
+        #endregion
+
+        #region INotifyDataErrorInfo
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+        
+        private Dictionary<string, IEnumerable<string>> validationErrors = new Dictionary<string, IEnumerable<string>>();
+
+        private void Validate(string propertyName)
+        {
+            var value = GetType().GetProperty(propertyName).GetValue(this);
+            var context = new ValidationContext(this) { MemberName = propertyName };
+            var errors = new List<ValidationResult>();
+       
+            validationErrors.Remove(propertyName);
+            if (Validator.TryValidateProperty(value, context, errors) == false)
+            {
+                validationErrors.Add(
+                    propertyName, 
+                    errors.Select(e => e.ErrorMessage).ToList().AsReadOnly()
+                );
+            }
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        public bool HasErrors => validationErrors.SelectMany(error => error.Value).Count() > 0;
+
+        public System.Collections.IEnumerable GetErrors(string propertyName) => validationErrors.ContainsKey(propertyName) ? validationErrors[propertyName] : Enumerable.Empty<string>();
         #endregion
 
         public ContentDialogResult Result { get; set; }  
